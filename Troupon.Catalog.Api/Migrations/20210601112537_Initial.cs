68d52f59829d685b287163c13c354ebbe56e9c21
Carl Montplaisir <carl.montplaisir@beslogic.com>
2021-06-01 07:42:36 -0400

    Prepared for Dapper and fix folders

---
+++ Troupon.Catalog.Api/Migrations/20210601112537_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Troupon.Catalog.Api.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Troupon.Catalog");
+
+            migrationBuilder.CreateTable(
+                name: "Deals",
+                schema: "Troupon.Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Limitation = table.Column<int>(type: "int", nullable: false),
+                    OtherConditions = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deals",
+                schema: "Troupon.Catalog");
+        }
+    }
+}
--- Troupon.Catalog.Core.Domain/Entities/Deal/Deal.cs
+++ Troupon.Catalog.Core.Domain/Entities/Deal/Deal.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Infra.DomainDrivenDesign.Base;
-using Troupon.Catalog.Core.Domain.Entities.Category;
-using Troupon.Catalog.Core.Domain.Entities.Common;
 using Troupon.Catalog.Core.Domain.Enums;
 
 namespace Troupon.Catalog.Core.Domain.Entities.Deal
@@ -16,31 +12,11 @@ namespace Troupon.Catalog.Core.Domain.Entities.Deal
     public int Limitation { get; private set; }
     public string OtherConditions { get; private set; }
     public DealStatus Status { get; private set; }
-    public virtual Account.Account Account { get; private set; }
-    public virtual ICollection<DealOption> Options { get; private set; }
-    public virtual ICollection<DealCategory> Categories { get; private set; }
+    public float Price { get; private set; }
 
     public Deal()
     {
-      Options = new List<DealOption>();
-      Categories = new List<DealCategory>();
-    }
 
-    public DealOption GetOption(
-      Guid optionId)
-    {
-      return Options.SingleOrDefault(x => x.Id == optionId);
-    }
-
-    public DealPrice GetOptionPrice(
-      Guid optionId,
-      Currency currency)
-    {
-      var option = GetOption(optionId);
-
-      if (option == null) return null;
-
-      return option.GetPrice(currency);
     }
   }
 }
