7758614af965aec01590690748a415b2ebdf5efa
Nicolas Charette-Naud <nicolas.charette-naud@beslogic.com>
2021-10-05 20:05:17 -0400

    feat: Make the strike 1 of Paging system

--- Troupon.Catalog.Core.Domain/InputModels/SearchDealInput.cs
+++ Troupon.Catalog.Core.Domain/InputModels/SearchDealInput.cs
@@ -1,15 +1,50 @@
-using System;
-using System.ComponentModel.DataAnnotations;
+using System.Collections;
+using System.Collections.Generic;
 using Troupon.Catalog.Core.Domain.Entities.Common;
-using Troupon.Catalog.Core.Domain.Enums;
 
 namespace Troupon.Catalog.Core.Domain.InputModels
 {
-  public record SearchDealsFilter(
-      string searchText,
+  public class SearchDealsFilter : PageableParameter
+  {
+    public string SearchText { get; }
+    public string Location { get; }
+    public IEnumerable<string> Categories { get; }
+    public IEnumerable<MinMax> PriceRanges { get; }
+    public IEnumerable<MinMax> DistanceRanges { get; }
+
+    public SearchDealsFilter(string searchText,
       string location,
-      string[] categories,
-      MinMax[] priceRanges,
-      MinMax[] distanceRanges
-    );
+      IEnumerable<string> categories,
+      IEnumerable<MinMax> priceRanges,
+      IEnumerable<MinMax> distanceRanges,
+      PagingInfo? pagingInfo) : base(pagingInfo)
+    {
+      SearchText = searchText;
+      Location = location;
+      Categories = categories;
+      PriceRanges = priceRanges;
+      DistanceRanges = distanceRanges;
+    }
+  }
+
+  public abstract class PageableParameter : IPageable
+  {
+    public PagingInfo PagingInfo { get; }
+
+    public PageableParameter(PagingInfo? info)
+    {
+      PagingInfo = info ?? new PagingInfo();
+    }
+  }
+
+  public interface IPageable
+  {
+    PagingInfo PagingInfo { get; }
+  }
+
+  public class PagingInfo
+  {
+    public int PageNumber { get; set; } = 0;
+    public int PageSize { get; set; } = 50;
+  }
 }
