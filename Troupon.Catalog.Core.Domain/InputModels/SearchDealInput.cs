391d203f8bfacc31a5e762fc667e34fd7e838aac
Nicolas Charette-Naud <nicolas.charette-naud@beslogic.com>
2021-10-07 11:16:14 -0400

    refactor: remove paging concept

--- Troupon.Catalog.Core.Domain/InputModels/SearchDealInput.cs
+++ Troupon.Catalog.Core.Domain/InputModels/SearchDealInput.cs
@@ -4,7 +4,7 @@ using Troupon.Catalog.Core.Domain.Entities.Common;
 
 namespace Troupon.Catalog.Core.Domain.InputModels
 {
-  public class SearchDealsFilter : PageableParameter
+  public class SearchDealsFilter
   {
     public string SearchText { get; }
     public string Location { get; }
@@ -12,12 +12,12 @@ namespace Troupon.Catalog.Core.Domain.InputModels
     public IEnumerable<MinMax> PriceRanges { get; }
     public IEnumerable<MinMax> DistanceRanges { get; }
 
-    public SearchDealsFilter(string searchText,
+    public SearchDealsFilter(
+      string searchText,
       string location,
       IEnumerable<string> categories,
       IEnumerable<MinMax> priceRanges,
-      IEnumerable<MinMax> distanceRanges,
-      PagingInfo? pagingInfo) : base(pagingInfo)
+      IEnumerable<MinMax> distanceRanges)
     {
       SearchText = searchText;
       Location = location;
@@ -25,26 +25,5 @@ namespace Troupon.Catalog.Core.Domain.InputModels
       PriceRanges = priceRanges;
       DistanceRanges = distanceRanges;
     }
-  }
-
-  public abstract class PageableParameter : IPageable
-  {
-    public PagingInfo PagingInfo { get; }
-
-    public PageableParameter(PagingInfo? info)
-    {
-      PagingInfo = info ?? new PagingInfo();
-    }
-  }
-
-  public interface IPageable
-  {
-    PagingInfo PagingInfo { get; }
-  }
-
-  public class PagingInfo
-  {
-    public int PageNumber { get; set; } = 0;
-    public int PageSize { get; set; } = 50;
   }
 }
