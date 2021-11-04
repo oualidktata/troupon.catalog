743e705e30e04632237c5867fcb4dcf33f6c946a
Nicolas Charette Naud <nicolas.charette-naud@beslogic.com>
2021-11-03 20:35:00 -0400

    refactor: Bring back HealthChecks from deal.management to catalog
    This will be in preparation for next sprint. We want to use Catalog as a templace for creating new microservices

--- Troupon.Catalog.Api/Startup.cs
+++ Troupon.Catalog.Api/Startup.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using HealthChecks.UI.Client;
 using Infra.Api.DependencyInjection;
 using Infra.Authorization.Policies;
 using Infra.MediatR;
@@ -8,8 +9,10 @@ using Infra.Persistence.Dapper.Extensions;
 using Infra.Persistence.EntityFramework.Extensions;
 using Infra.Persistence.SqlServer.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +21,7 @@ using Troupon.Catalog.Api.DependencyInjectionExtensions;
 using Troupon.Catalog.Api.ToMoveOrRemove;
 using Troupon.Catalog.Core.Application.Queries.Deals;
 using Troupon.Catalog.Infra.Persistence;
+using Troupon.DealManagement.Api.ToMoveOrRemove;
 
 namespace Troupon.Catalog.Api
 {
@@ -65,6 +69,11 @@ namespace Troupon.Catalog.Api
 
       services.AddPwcApiBehaviour();
 
+      // TO MOVE ?
+      services.AddHealthChecks(Configuration);
+      services.AddHealthChecksUI();
+      services.AddFluentValidaton();
+
       // TO REMOVE ?
       services.AddDapperPersistence("mainDatabaseConnStr");
     }
@@ -89,7 +98,54 @@ namespace Troupon.Catalog.Api
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
+        ConfigureHealthChecks(endpoints);
       });
+    }
+
+    private void ConfigureHealthChecks(IEndpointRouteBuilder endpoints)
+    {
+      endpoints.MapHealthChecks(
+            "/health",
+            new HealthCheckOptions()
+            {
+              Predicate = (
+                check) => check.Tags.Contains("all"),
+              ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+            });
+      endpoints.MapHealthChecks(
+        "/health/external",
+        new HealthCheckOptions()
+        {
+          Predicate = (
+            check) => check.Tags.Contains("external"),
+          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+        });
+      endpoints.MapHealthChecks(
+        "/health/db",
+        new HealthCheckOptions()
+        {
+          Predicate = (
+            check) => check.Tags.Contains("db"),
+          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+        });
+      endpoints.MapHealthChecks(
+        "/health/uri",
+        new HealthCheckOptions()
+        {
+          Predicate = (
+            check) => check.Tags.Contains("uri"),
+          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+        });
+      endpoints.MapHealthChecks(
+        "/health/internal",
+        new HealthCheckOptions()
+        {
+          Predicate = (
+            check) => check.Tags.Contains("errors") || check.Tags.Contains("db"),
+          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+        });
+
+      endpoints.MapHealthChecksUI();
     }
   }
 }
